e9651b8b433385a0b0e6703abb615a74b394134b
kaushiki <kaushiki.ak88@gmail.com>
2021-01-21 17:58:33 +0530

    Employee attendance

--- Employeewage.java
+++ Employeewage.java
@@ -1,6 +1,19 @@
-public class Employeewage{
-	public static void main(String[] args)
-	{
-		System.out.println("Welcome to Employee wage computation");
+public class Employeewage
+{
+	private void doCalculation()
+  {
+		System.out.println("Welcome to Employee Wage Builder");
+  // contents
+  		int is_Full_Time = 1;
+    //computation
+  		double empCheck = Math.floor(Math.random() * 10) % 2;
+  		if (empCheck == is_Full_Time)
+    		System.out.println("Employee is present ");
+  		else
+   		 System.out.println("Employee is Abscent");
+	}
+	public static void main(String[] args){
+  		Employeewage employeewage = new Employeewage();
+  		employeewage.doCalculation();
 	}
 }
