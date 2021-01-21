454957cfb8151fceb0866df26e14c2a92f76470d
kaushiki <kaushiki.ak88@gmail.com>
2021-01-21 19:41:09 +0530

    Employee Wages for one month

--- Employeewage.java
+++ Employeewage.java
@@ -9,22 +9,19 @@ public class Employeewage
 		int Wage_per_hour = 20;
 		int no_of_hours = 8;
 		int no_of_part_time_hour = 4;
+		int days = 20;
 		//computation
-		int empCheck =(int) (Math.floor(Math.random() * 10) % 3);
+		int empCheck =(int) (Math.floor(Math.random() * 10) % 2);
 		switch (empCheck){
 		case 0:
 			System.out.println("Employee is present ");
-			int Payment = Wage_per_hour * no_of_hours;
+			int Payment = Wage_per_hour * no_of_hours * days;
 			System.out.println("Payment is: "+ Payment);
 			break;
 		case 1:
 			System.out.println("Employee is part time");
-			int Payments = Wage_per_hour * no_of_part_time_hour;
+			int Payments = Wage_per_hour * no_of_part_time_hour * days;
 			System.out.println("Part time Payment is: "+ Payments);
-			break;
-		case 2:
-			System.out.println("Employee is Abscent");
-			System.out.println("Payment is 0");
 			break;
 		}
 	}
