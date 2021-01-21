b3d6acbb981cd8aa8cc76b5fa1f756ada1044131
kaushiki <kaushiki.ak88@gmail.com>
2021-01-21 19:14:54 +0530

    Finding Employee wage using switch case

--- Employeewage.java
+++ Employeewage.java
@@ -3,29 +3,30 @@ public class Employeewage
 	private void doCalculation()
   {
 	System.out.println("Welcome to Employee Wage Builder");
-		// contents
-		int is_Full_Time = 1;
+	  // contents
+  		int is_Full_Time = 1;
 		int is_Part_Time = 2;
 		int Wage_per_hour = 20;
 		int no_of_hours = 8;
 		int no_of_part_time_hour = 4;
-		//computation
-		int empCheck =(int) (Math.floor(Math.random() * 10) % 3);
-		switch (empCheck){
-		case 0:
-			System.out.println("Employee is present ");
-			int Payment = Wage_per_hour * no_of_hours;
+    //computation
+  		double empCheck = Math.floor(Math.random() * 10) % 3;
+  		if (empCheck == is_Full_Time)
+		{
+    		System.out.println("Employee is present ");
+  			int Payment = Wage_per_hour * no_of_hours;
 			System.out.println("Payment is: "+ Payment);
-			break;
-		case 1:
+		}
+		else if (empCheck == is_Part_Time){
 			System.out.println("Employee is part time");
-			int Payments = Wage_per_hour * no_of_part_time_hour;
-			System.out.println("Part time Payment is: "+ Payments);
-			break;
-		case 2:
+			int Payment = Wage_per_hour * no_of_part_time_hour;
+			System.out.println("Payment is: "+ Payment);
+		}
+		else
+		{
 			System.out.println("Employee is Abscent");
 			System.out.println("Payment is 0");
-			break;
+		}
 		}
 	}
 	public static void main(String[] args)
