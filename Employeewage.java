30bd4cabd8a5d182999e90c74c3fec9f03105e42
kaushiki <kaushiki.ak88@gmail.com>
2021-01-21 19:46:52 +0530

    Employee wage for 20 days

--- Employeewage.java
+++ Employeewage.java
@@ -2,27 +2,21 @@ public class Employeewage
 {
 	private void doCalculation()
   {
-	System.out.println("Welcome to Employee Wage Builder");
+	System.out.println("Welcome to Employee Wage Computation");
 		// contents
 		int is_Full_Time = 1;
-		int is_Part_Time = 2;
 		int Wage_per_hour = 20;
-		int no_of_hours = 8;
-		int no_of_part_time_hour = 4;
+		int no_of_hours = 100;
 		int days = 20;
 		//computation
 		int empCheck =(int) (Math.floor(Math.random() * 10) % 2);
-		switch (empCheck){
-		case 0:
+		if(empCheck == is_Full_Time){
 			System.out.println("Employee is present ");
 			int Payment = Wage_per_hour * no_of_hours * days;
-			System.out.println("Payment is: "+ Payment);
-			break;
-		case 1:
-			System.out.println("Employee is part time");
-			int Payments = Wage_per_hour * no_of_part_time_hour * days;
-			System.out.println("Part time Payment is: "+ Payments);
-			break;
+			System.out.println("Full time Payment for one month: "+ Payment);
+		}
+		else{
+			System.out.println("Employee is Abscent");
 		}
 	}
 	public static void main(String[] args)
